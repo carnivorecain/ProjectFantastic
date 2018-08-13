65ca0c23a04e116ca7991f90ab935cf81cdcc24b
Christine Cain <ccain2154@gmail.com>
2018-08-13 21:51:09 +0100

    added objects to track controllers

---
+++ Assets/FollowParent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowParent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = Vector3.zero;
+
+    }
+}
