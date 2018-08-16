301947bb3909d4e61cd32bd6ec61b40fee8dbc7b
Christine Cain <ccain2154@gmail.com>
2018-08-16 23:39:58 +0100

    added TwirlOnCollision, made Ceiling prefab

---
+++ Assets/Scripts/Ceiling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ceiling : MonoBehaviour {
+
+    public float playerHeight = 2f;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        GameObject floor = GameObject.Find("Floor");
+        GameObject ceiling = Instantiate(Resources.Load("Ceiling")) as GameObject;
+        ceiling.transform.position = floor.transform.position;
+        ceiling.transform.rotation = floor.transform.rotation;
+        float ceilingHeight = playerHeight * 0.75f + 1f; // +1 for lift height
+        ceiling.transform.Translate(Vector3.up * ceilingHeight);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+    
+	}
+}
---
+++ Assets/Scripts/TwirlOnCollision.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class TwirlOnCollision : MonoBehaviour {
+
+    Twirl twirl;
+    float maxAngle = 30;
+    bool twirling = false;
+    float speed = 10f;
+    float curAngle = 0f;
+
+    // Use this for initialization
+    void Start () {
+        twirl = GetComponent<Twirl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (twirling)
+        {
+            curAngle = curAngle += maxAngle * Time.deltaTime * speed;
+        }
+        else
+        {
+            curAngle = 0f;
+        }
+        twirl.angle = curAngle;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CauseTwirl") { 
+            twirling = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "CauseTwirl") {
+            twirling = false;
+        }
+    }
+
+
+}
