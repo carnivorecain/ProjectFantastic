0cedfe8eeb9c959d0882827b8800a8d47134b5de
Christine Cain <ccain2154@gmail.com>
2018-08-26 12:10:44 +0100

    Take 2

--- Assets/Scripts/TrackObjectHeight.cs
+++ Assets/Scripts/TrackObjectHeight.cs
@@ -7,57 +7,99 @@ public class TrackObjectHeight : MonoBehaviour {
     private RaycastHit hit;
     public GameObject trackedObject;
     int groundCount = 0;
+    public bool isGrounded = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 
-    public bool isGrounded () {
-        print(name + " ground count " + groundCount);
-        return groundCount > 0;
-    }
+    // TODO: rewrite to check for an intersection with GetHitObject
+    //public bool isGrounded () {
+    //    print(name + " ground count " + groundCount);
+    //    return groundCount > 0;
+    //}
 
     // Update is called once per frame
     void Update () {
-        
+
         Vector3 scale = transform.localScale;
         scale.y = trackedObject.transform.localPosition.y;
         transform.localScale = scale;
 
         Vector3 position = transform.position;
         position.x = trackedObject.transform.position.x;
-        position.y = trackedObject.transform.position.y - scale.y;
+        position.y = trackedObject.transform.position.y - transform.lossyScale.y / 2f;
         position.z = trackedObject.transform.position.z;
         transform.position = position;
 
         transform.rotation = Quaternion.identity;
     }
 
-    bool GetHitDistance(out float distance)
-    {
+    //bool GetHitFromTrackedObject(out float distance) {
+    bool GetHitDistance(out float distance) {
         distance = 0f;
-        Ray downRay = new Ray(transform.position, -Vector3.up); // this is the downward ray
-        if (Physics.Raycast(downRay, out hit))
+
+        // Bit shift the index of the Surface layer (9) to get a bit mask
+        int layerMask = 1 << 9;
+        // This would cast rays only against colliders in layer 9.
+
+        RaycastHit hit;
+        Vector3 pos = trackedObject.transform.position;
+        Vector3 dir = Vector3.down;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(pos, dir, out hit, Mathf.Infinity, layerMask))
         {
+            Debug.DrawRay(pos, dir * hit.distance, Color.green);
             distance = hit.distance;
-            // print("distance:" + distance);
+            print(name + " distance: " + distance);
             return true;
         }
-        return false;
+        else
+        {
+            Debug.DrawRay(pos, dir * 1000, Color.red);
+            Debug.Log(name + "Did not Hit");
+            return false;
+        }
+
     }
 
-    public float getRelativeHeight()
+    public GameObject GetHitObject()
+    {
+        // Bit shift the index of the Surface layer (9) to get a bit mask
+        int layerMask = 1 << 9;
+        // This would cast rays only against colliders in layer 9.
+
+        RaycastHit hit;
+        Vector3 pos = trackedObject.transform.position;
+        Vector3 dir = Vector3.down;
+        // Does the ray intersect any objects excluding the player layer
+        //    Ray downRay = new Ray(transform.position, -Vector3.up); // this is the downward ray
+        if (Physics.Raycast(pos, dir, out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(pos, dir * hit.distance, Color.green);
+            return hit.transform.gameObject;
+        }
+        else
+        {
+            Debug.DrawRay(pos, dir * 1000, Color.red);
+            Debug.Log(name + "Did not Hit");
+            return null;
+        }
+
+    }
+
+    public float GetRelativeHeight()
     {
         return transform.position.y - transform.lossyScale.y/2f;
     }
 
-    public float getHeight()
+    public float GetHeight()
     {
         var dist = 0f;
         if (GetHitDistance(out dist))
         {
-            var height = dist - getRelativeHeight();
+            var height = dist - GetRelativeHeight();
             print("current height:" + height);
             return height;
         }
@@ -66,19 +108,21 @@ public class TrackObjectHeight : MonoBehaviour {
 
     protected virtual void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag != "CauseTwirl")
+        if (collision.gameObject.tag == "Surface")
         {
             print(this.name + " collided with " + collision.gameObject.name);
             groundCount++;
+            isGrounded = true;
         }
     }
 
     protected virtual void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.tag != "CauseTwirl")
+        if (collision.gameObject.tag == "Surface")
         {
             print(this.name + " exitted " + collision.gameObject.name);
             groundCount--;
+            isGrounded = false;
         }
     }
 }
---
+++ Assets/Scripts/VRHeightManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class VRHeightManager : MonoBehaviour {
+
+    public float gravity = -.02f;
+    private float velocity = 0f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (IsGrounded())
+        {
+            GameObject surface = GetSurfaceObject();
+            if (surface.transform.position.y > transform.position.y)
+            {
+                velocity = 1f;
+            }
+        }
+        else
+        {
+            ApplyGravity();
+        }
+
+        CalculateMovement();
+    }
+
+    //bool Intersects(Bounds bounds)
+    //{
+    //    overlapping = collider.bounds.Intersects(bounds);
+    //    return overlapping;
+    //}
+
+    bool IsGrounded()
+    {
+
+
+
+        GameObject[] headsets = GameObject.FindGameObjectsWithTag("HeadsetTracker");
+        GameObject[] controllers = GameObject.FindGameObjectsWithTag("ControllerTracker");
+
+        //combine into one array 
+        GameObject[] heightTrackers = headsets.Concat(controllers).ToArray();
+
+        for (int i = 0; i < heightTrackers.Length; i++)
+        {
+            GameObject heightTracker = heightTrackers[i];
+            TrackObjectHeight tracker = heightTracker.GetComponent<TrackObjectHeight>();
+            if (tracker != null && tracker.isGrounded)
+            {
+                print(heightTracker.name + " is grounded");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    GameObject GetSurfaceObject()
+    {
+        //find body and feet 
+        GameObject headset = GameObject.FindGameObjectsWithTag("HeadsetTracker")[0];
+        TrackObjectHeight tracker = headset.GetComponent<TrackObjectHeight>();
+        GameObject surface = tracker.GetHitObject();
+        
+        return surface;
+    }
+
+    //Bounds AllBounds()
+    //{
+    //    this.transform.bou
+    //    Bounds bounds = renderer.bounds;
+    //    foreach (var r in GetComponentsInChildren<Renderer>())
+    //    {
+    //        bounds.Encapsulate(r.bounds);
+    //    }
+    //    return bounds
+    //}
+
+    void ApplyGravity()
+    {
+        velocity += gravity * Time.deltaTime;
+
+        // cap at a terminal velocity
+        if (velocity > 10f) velocity = 10f;
+    }
+
+    void CalculateMovement()
+    {
+        print("velocity:" + velocity);
+        if (Mathf.Abs(velocity) > 0.01f)
+        {
+            var fallDist = velocity * Time.deltaTime;
+            transform.Translate(Vector3.up * fallDist);
+        }
+    }
+}
